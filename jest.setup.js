fbda66281f47c27350885b0a4dc764db0277421f
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-17 12:06:54 -0400

    chore: Changes for basic javascript testing.

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,1 @@
-import { configure, shallow, render, mount } from "enzyme"
-import Adapter from "enzyme-adapter-react-16"
-
-configure({ adapter: new Adapter() })
-
-global.shallow = shallow
-global.render = render
-global.mount = mount
+import { configure } from "enzyme"
