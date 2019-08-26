738ec54683d94569b1eefe991bcd1e943935dd1a
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-08-26 18:39:15 -0400

    feat: Adds new util methods.

--- src/utils.js
+++ src/utils.js
@@ -1,29 +1,51 @@
 import _ from "underscore"
 
-export function makeUppercaseUnderscored(string) {
-  const uppercased = string.toUpperCase()
-  const underscored = uppercased.replace(/-/g, "_")
-  return underscored
+const pattern = "[A-Za-z]+"
+const camelCaseRegex = /(?:^|\.?)([A-Z])/g
+const dashCaseRegex = new RegExp(`^${pattern}(-${pattern})+$`)
+const underscoreCaseRegex = new RegExp(`^${pattern}(_${pattern})+$`)
+
+const isUnderscoreCase = string => underscoreCaseRegex.test(string)
+const isDashCase = string => dashCaseRegex.test(string)
+const isCamelCase = string => camelCaseRegex.test(string)
+
+const fromDashCase = (string, newCase) => string.replace(/-/g, newCase)
+const fromUnderscoreCase = (string, newCase) => string.replace(/_/g, newCase)
+function fromCamelCase(string, newCase) {
+  const handler = (_, y) => newCase + y.toLowerCase()
+  const prefix = new RegExp(`^${newCase}`)
+  return string.replace(camelCaseRegex, handler).replace(prefix, "")
+}
+
+export const makeUppercaseUnderscored = string => fromDashCase(string, "_").toUpperCase()
+export const makeLowercaseDashed = string => {
+  if (isUnderscoreCase(string)) {
+    return fromUnderscoreCase(string, "-").toLowerCase()
+  }
+  return fromCamelCase(string, "-").toLowerCase()
 }
 
 export function createSuffixes(names) {
   const built = []
 
   names.forEach(name => {
-    if (_.isArray(name)) {
+    if (_.isString(name)) {
+      built.push(makeUppercaseUnderscored(name))
+    }
+    else if (_.isArray(name)) {
       const base = name[0]
       createSuffixes(name[1]).forEach(uppercase => {
-        const formatted = makeUppercaseUnderscored(`${base}_${uppercase}`)
-        built.push(formatted)
+        built.push(makeUppercaseUnderscored(`${base}_${uppercase}`))
       })
-    }
-    else if (_.isString(name)) {
-      const formatted = makeUppercaseUnderscored(name)
-      built.push(formatted)
     }
   })
 
   return built
 }
 
-
+export function normalize(name) {
+  if (name.startsWith("/")) {
+    name = name.replace("/", "")
+  }
+  return name
+}
