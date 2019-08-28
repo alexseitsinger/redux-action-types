4011193e7bd856311c2c12d329db32a88fdc112d
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-08-27 20:29:40 -0400

    fix: Returns wrapped in methods object.
    Renamed module/method. Fixes export statement.

--- src/createMapDispatchForSections.js
+++ src/createMapDispatchForSections.js
@@ -2,24 +2,28 @@ import _ from "underscore"
 import { bindActionCreators } from "redux"
 
 
-export function mapDispatchForSections(sections, mapMethod) {
+export function createMapDispatchForSections(sections, mapMethod) {
   return dispatch => {
-    const mapped = {}
+    const methods = {}
 
     // convert each section provided into bound action creators.
     Object.keys(sections).forEach(sectionName => {
       const actionCreators = sections[sectionName]
-      mapped[sectionName] = bindActionCreators({...actionCreators}, dispatch)
+      methods[sectionName] = bindActionCreators({...actionCreators}, dispatch)
     })
 
     // If we also get a mapper function, invoke it to add to the state.
     if (_.isFunction(mapMethod)) {
       return {
-        ...mapped,
-        ...mapMethod(dispatch),
+        methods: {
+          ...methods,
+          ...mapMethods(dispatch),
+        }
       }
     }
 
-    return mapped
+    return {
+      methods: methods,
+    }
   }
 }
