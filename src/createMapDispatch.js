6b00e8c459074e788eebe5a4bf76f531b9e1b31c
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-08-31 13:15:17 -0400

    docs: Adds JSDoc.

--- src/createMapDispatch.js
+++ src/createMapDispatch.js
@@ -1,7 +1,30 @@
 import _ from "underscore"
 import { bindActionCreators } from "redux"
 
-
+/**
+ * Creates a mapDispatch function for the actionType sections.
+ *
+ * @param {string} [prefix]
+ * The page name to save each method into.
+ * @param {object} sections
+ * The actionType sections actions.
+ * @param {function} mapMethod
+ * An extra method to run to create extra dispatched methods.
+ *
+ * @returns {function}
+ * A function that maps dispatch to each action provided for each section.
+ *
+ * @example
+ * import { createMapDispatch } from "@alexseitsinger/redux-action-types"
+ *
+ * import * as usersActions from "./actions/users"
+ * import * as tasksActions from "./actions/tasks"
+ *
+ * export const mapDispatch = createMapDispatch("home-page", {
+ *   users: usersActions,
+ *   tasks: tasksActions,
+ * })
+ */
 export function createMapDispatch(prefix, sections, mapMethod) {
   if (_.isArray(prefix)) {
     mapMethod = sections
--- src/createReducer.js
+++ src/createReducer.js
@@ -1,5 +1,38 @@
 import { reducerFactory } from "./reducerFactory"
 
+/**
+ * Creates a reducer for each actionType section.
+ *
+ * @param {object} initialState
+ * The initial state to use for the reducer.
+ * @param {object} sections
+ * The actionType sections to create a reducer for.
+ * @param {object} reducers
+ * The reducers to use for each section.
+ *
+ * @returns {function}
+ * A reducer function that delegates actions to the appropriate section reducer.
+ *
+ * @example
+ * // pages/home/reducer/index.js
+ * import { createReducer } from "@alexseitsinger/redux-action-types"
+ * import { actionTypeSections } from "../types"
+ * import initialState from "./state.json"
+ * // or
+ * const initialState = {
+ *   users: {
+ *     ...
+ *   },
+ *   tasks: {
+ *     ...
+ *   }
+ * }
+ *
+ * export const homePageReducer = createReducer(initialState, actionTypeSections, {
+ *   users: usersReducer,
+ *   tasks: tasksReducer,
+ * })
+ */
 export function createReducer(initialState, sections, reducers) {
   const { reducer, addCase, cases } = reducerFactory(initialState)
 
