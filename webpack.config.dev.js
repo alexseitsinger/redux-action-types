2a9ad3c5b5d7daee9368d9bf029ad0915d8dc12f
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-09-29 13:13:06 -0400

    chore: Adds webpack dev config.

---
+++ webpack.config.dev.js
@@ -0,0 +1,23 @@
+const path = require("path")
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  target: "node",
+  devtool: "source-map",
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].dev.js",
+    sourceMapFilename: "[name].dev.js.map",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  },
+}
