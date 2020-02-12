70768c127667eaa964c340e5cd049d43d41efb44
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-02-12 08:58:27 -0500

    test: Updates tests.

--- tests/createActionTypes.test.ts
+++ tests/createActionTypes.test.ts
@@ -8,13 +8,17 @@ describe("createActionTypes", () => {
         "data",
         ["login-form", ["submitted", ["completed", ["success", "failure"]]]],
         [
-          "three",
+          "activation-form",
           [
-            ["alpha", ["one", "two"]],
-            ["beta", ["one"]],
+            ["first-action", ["a", "b"]],
+            ["second-action", ["a"]],
           ],
         ],
-        "four",
+        "authentication-form",
+        [
+          "signup-form",
+          ["first-action", ["second-action", ["a", ["b", ["one", "two"]]]]],
+        ],
       ],
     })
 
@@ -23,10 +27,19 @@ describe("createActionTypes", () => {
       LOGIN_FORM_SUBMITTED: "@@page-name/login-form/SUBMITTED",
       LOGIN_FORM_COMPLETED_SUCCESS: "@@page-name/login-form/completed/SUCCESS",
       LOGIN_FORM_COMPLETED_FAILURE: "@@page-name/login-form/completed/FAILURE",
-      THREE_ALPHA_ONE: "@@page-name/three/alpha/ONE",
-      THREE_ALPHA_TWO: "@@page-name/three/alpha/TWO",
-      THREE_BETA_ONE: "@@page-name/three/beta/ONE",
-      FOUR: "@@page-name/FOUR",
+      ACTIVATION_FORM_FIRST_ACTION_A:
+        "@@page-name/activation-form/first-action/A",
+      ACTIVATION_FORM_FIRST_ACTION_B:
+        "@@page-name/activation-form/first-action/B",
+      ACTIVATION_FORM_SECOND_ACTION_A:
+        "@@page-name/activation-form/second-action/A",
+      AUTHENTICATION_FORM: "@@page-name/AUTHENTICATION_FORM",
+      SIGNUP_FORM_FIRST_ACTION: "@@page-name/signup-form/FIRST_ACTION",
+      SIGNUP_FORM_SECOND_ACTION_A: "@@page-name/signup-form/second-action/A",
+      SIGNUP_FORM_SECOND_ACTION_B_ONE:
+        "@@page-name/signup-form/second-action/b/ONE",
+      SIGNUP_FORM_SECOND_ACTION_B_TWO:
+        "@@page-name/signup-form/second-action/b/TWO",
     })
   })
 })
